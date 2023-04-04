7e4148d2a0a0afe9f5449d7e93418e4af376c76e
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-04-04 10:34:41 +0300

    fixed button mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,44 @@
 import React from "react";
-import logo from '../images/logo.svg';
-import HeaderNav from "./HeaderNav";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
 
 function Header(props) {
-      return(
-      <header className="header page__conteiner">
-        <img
-          className="header__logo"
-          src={logo}
-          alt="Логотип Место"
-        />
-        <HeaderNav  
-          email={props.email}
+  const [isMenuOpen, setIsMenuOpen] = React.useState("false");
+  const headerLinkClassName = `header__link ${
+    props.loggedIn
+      ? "header__link_type_login header__container_hiden"
+      : "header__link_type_logout"
+  }`;
+  const navButtonMenuClassName = `header__nav-icon ${
+    !isMenuOpen && "header__nav-icon_close"
+  }`;
+
+  function handleMenuOpen() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <header className="header page__conteiner">
+      <img className="header__logo" src={logo} alt="Логотип Место" />
+      <div className="header__container">
+        {props.loggedIn && (
+          <button className="header__nav-button" onClick={handleMenuOpen}>
+            <span className={navButtonMenuClassName}></span>
+          </button>
+        )}
+        {props.loggedIn && (
+          <p className="header__email header__container_hiden">{props.email}</p>
+        )}
+        <Link
+          to={`/${props.to}`}
           onClick={props.onClick}
-          text={props.text}
-          to={props.to}
-          loggedIn={props.loggedIn}/>
-      </header>
-    )
+          className={headerLinkClassName}
+        >
+          {props.text}
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
