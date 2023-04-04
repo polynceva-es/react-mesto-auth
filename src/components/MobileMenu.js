c36979b1400e3dcc4e0f5cec063e964ee60d515a
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-04-04 19:11:51 +0300

    MobileMenu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -3,56 +3,29 @@ import { Link } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 function MobileMenu(props) {
-
-//   const headerLinkClassName = `header__link ${
-//     props.loggedIn
-//       ? "header__link_type_login"
-//       : "header__link_type_logout"
-//   }`;
   const navButtonMenuClassName = `header__nav-icon ${
     props.isMenuOpen && "header__nav-icon_close"
   }`;
 
   return (
-    // <header className="header page__conteiner">
-    //   <img className="header__logo" src={logo} alt="Логотип Место" />
-    //   <div className="header__container">
-    //     {props.loggedIn && (
-    //       <button className="header__nav-button" onClick={props.handleMenuOpen}>
-    //         <span className={navButtonMenuClassName}></span>
-    //       </button>
-    //     )}
-    //     {props.loggedIn && (
-    //       <p className='header__email header__email-mobile'>{props.email}</p>
-    //     )}
-    //     <Link
-    //       to={`/${props.to}`}
-    //       onClick={props.onClick}
-    //       className={headerLinkClassName}
-    //     >
-    //       {props.text}
-    //     </Link>
-    //   </div>
-    // </header>
-    <header className="header_mobile page__conteiner">
-     <div className="header__conteiner_mobile_column">
-        <p className='header__email-mobile'>{props.email}</p>
+    <header className="header_type_mobile page__conteiner">
+      <div className="header__container_mobile_column">
+        <p className="header__email_mobile">{props.email}</p>
         <Link
-            to={`/${props.to}`}
-            onClick={props.onClick}
-            className="header__link_mobile"
-            >
-            {props.text}
+          to={`/${props.to}`}
+          onClick={props.onClick}
+          className="header__link_mobile"
+        >
+          {props.text}
         </Link>
-     </div>
+      </div>
 
-    <div className="header__conteiner_mobile_row">
+      <div className="header__container_mobile_row">
         <img className="header__logo" src={logo} alt="Логотип Место" />
         <button className="header__nav-button" onClick={props.handleMenuOpen}>
-            <span className={navButtonMenuClassName}></span>
+          <span className={navButtonMenuClassName}></span>
         </button>
-    </div>
-
+      </div>
     </header>
   );
 }
